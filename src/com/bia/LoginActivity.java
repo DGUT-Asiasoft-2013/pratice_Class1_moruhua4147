7ce29462249797c9a60289b1809dd0bcdbb9a48c
Administrator <Administrator@7A304-0028>
2016-12-06 10:02:05 +0800

    填充了登陆界面内容
    2016.12.6
    10:02

--- src/com/bia/LoginActivity.java
+++ src/com/bia/LoginActivity.java
@@ -1,17 +1,25 @@
 package com.bia;
 
+import com.bia.fragment.SimpleTextInputCellFragment;
+
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+	SimpleTextInputCellFragment fraginputid;
+	SimpleTextInputCellFragment fraginputpassword;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		fraginputid=(SimpleTextInputCellFragment) getFragmentManager().findFragmentById(R.id.input_id);
+		fraginputpassword=(SimpleTextInputCellFragment) getFragmentManager().findFragmentById(R.id.input_password);
 
 		findViewById(R.id.btn_register).setOnClickListener(new View.OnClickListener() {
 
@@ -20,10 +28,44 @@ public class LoginActivity extends Activity {
 				goRegister();
 			}
 		});
+		
+		findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				goLogin();
+				
+			}
+		});
+		
+		findViewById(R.id.for_password).setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "Õ¸º«¡Àƒ‹‘ı√¥∞Ï¬Ô",
+					     Toast.LENGTH_SHORT).show();				
+			}
+		});
+		
 		}
+	
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		fraginputid.setLabelText("’À∫≈");
+		fraginputid.setHintText("«Î ‰»Î’À∫≈");
+		fraginputpassword.setLabelText("√‹¬Î");
+		fraginputpassword.setHintText("«Î ‰»Î√‹¬Î");
+		
+	}
 
 	void goRegister() {
 		Intent itnt = new Intent(this, RegisterActivity.class);
 		startActivity(itnt);
+	}
+	void goLogin(){
+		Intent intent=new Intent(this, HelloWorldActivity.class);
+		startActivity(intent);
 	}
 }
